ff53150cede0afb64b3de966d6a8a80f1ca0b6b3
webbdm <gwebb762@gmail.com>
2017-04-11 17:41:45 -0500

    CSS edits

--- javascript/main.js
+++ javascript/main.js
@@ -12,19 +12,19 @@ $.ajax('./db/dinosaurs.json').done(function(data) {
 
 
 function makeDom(arrayToPrint) {
-	var counter = 0;
+    var counter = 0;
     var myDomString = "";
     var domRow = "";
     domRow += `<div class="row">`;
     for (var i = 0; i < arrayToPrint.length; i++) {
-        myDomString += `<div class="dinoCard col-md-3 thumbnail">`;
-        myDomString += `<header><h1>${arrayToPrint[i].type}</h1></header>`;
-        myDomString += `<section>`
-        myDomString += `<img class="image" src="${arrayToPrint[i].img}">`;
-        myDomString += `<p class="bio">${arrayToPrint[i].bio}</p>`;
-        myDomString += `</section>`;
-        myDomString += `<footer>${arrayToPrint[i].info}</footer>`;
-        myDomString += `</div>`;
+        domRow += `<div class="dinoCard col-md-3 thumbnail">`;
+        domRow += `<header><h1>${arrayToPrint[i].type}</h1></header>`;
+        domRow += `<section>`
+        domRow += `<img class="image" src="${arrayToPrint[i].img}">`;
+        domRow += `<p class="bio">${arrayToPrint[i].bio}</p>`;
+        domRow += `</section>`;
+        domRow += `<footer>${arrayToPrint[i].info}</footer>`;
+        domRow += `</div>`;
 
         counter += 1;
 
